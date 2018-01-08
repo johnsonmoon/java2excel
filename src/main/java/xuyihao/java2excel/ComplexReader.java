0ee90c4e2e7060012701f53af0795de482daf275
xuyh <xuyh@uyunsoft.cn>
2018-01-08 15:03:49 +0800

    实现ComplexReader的功能

--- src/main/java/xuyihao/java2excel/ComplexReader.java
+++ src/main/java/xuyihao/java2excel/ComplexReader.java
@@ -4,11 +4,15 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import xuyihao.java2excel.core.entity.model.Template;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * ComplexReader
- * TODO
- * <p>
  * <p>
  * <pre>
  * 	Read excel file at multiple sheets.
@@ -19,15 +23,218 @@ import org.slf4j.LoggerFactory;
 public class ComplexReader extends AbstractReader {
 	private static Logger logger = LoggerFactory.getLogger(ComplexReader.class);
 	private Workbook workbook;
-
-	public ComplexReader() {
-	}
+	private String filePathName;
+	private Map<Integer, Integer> templateSheetCurrentRowNumberMap = new HashMap<>();
+	private Map<Integer, Integer> typeSheetCurrentRowNumberMap = new HashMap<>();
 
 	public ComplexReader(String filePathName) {
-		openFile(filePathName);
+		this.filePathName = filePathName;
+		openWorkBook(filePathName);
 	}
 
-	public boolean openFile(String filePathName) {
+	private void setTemplateSheetCurrentRowNumber(int sheetNumber, int currentRowNumber) {
+		templateSheetCurrentRowNumberMap.put(sheetNumber, currentRowNumber);
+	}
+
+	private Integer getTemplateSheetCurrentRowNumber(int sheetNumber) {
+		if (!templateSheetCurrentRowNumberMap.containsKey(sheetNumber)) {
+			templateSheetCurrentRowNumberMap.put(sheetNumber, 0);
+			return 0;
+		} else {
+			return templateSheetCurrentRowNumberMap.get(sheetNumber);
+		}
+	}
+
+	private void setTyepSheetCurrentRowNumber(int sheetNumber, int currentRowNumber) {
+		typeSheetCurrentRowNumberMap.put(sheetNumber, currentRowNumber);
+	}
+
+	private Integer getTypeSheetCurrentRowNumber(int sheetNumber) {
+		if (!typeSheetCurrentRowNumberMap.containsKey(sheetNumber)) {
+			typeSheetCurrentRowNumberMap.put(sheetNumber, 0);
+			return 0;
+		} else {
+			return typeSheetCurrentRowNumberMap.get(sheetNumber);
+		}
+	}
+
+	/**
+	 * Read type info at sheet sheetNumber.
+	 *
+	 * @param sheetNumber sheet number
+	 * @return type (nullable)
+	 */
+	public Class<?> readExcelJavaClass(int sheetNumber) {
+		Class<?> clazz = null;
+		try {
+			if (workbook == null)
+				openWorkBook(filePathName);
+			clazz = readJavaClass(workbook, sheetNumber);
+		} catch (Exception e) {
+			logger.warn(e.getMessage(), e);
+		} finally {
+			close(workbook);
+		}
+		return clazz;
+	}
+
+	/**
+	 * Read template info at sheet sheetNumber.
+	 *
+	 * @param sheetNumber sheet number
+	 * @return template (nullable)
+	 */
+	public Template readExcelTemplate(int sheetNumber) {
+		Template template = null;
+		try {
+			if (workbook == null)
+				openWorkBook(filePathName);
+			template = readTemplate(workbook, sheetNumber);
+		} catch (Exception e) {
+			logger.warn(e.getMessage(), e);
+		} finally {
+			close(workbook);
+		}
+		return template;
+	}
+
+	/**
+	 * Get data count at sheet sheetNumber
+	 *
+	 * @param sheetNumber sheet number
+	 * @return data count
+	 */
+	public int readExcelDataCount(int sheetNumber) {
+		int dataCount = 0;
+		try {
+			if (workbook == null)
+				openWorkBook(filePathName);
+			dataCount = readDataCount(workbook, sheetNumber);
+		} catch (Exception e) {
+			logger.warn(e.getMessage(), e);
+		}
+		return dataCount;
+	}
+
+	/**
+	 * Read template data at sheet sheetNumber.
+	 *
+	 * @param sheetNumber sheet number
+	 * @param readSize    given size to read
+	 * @return template list of given size
+	 */
+	public List<Template> readExcelData(int sheetNumber, int readSize) {
+		List<Template> templates = new ArrayList<>();
+		if (readSize <= 0)
+			return templates;
+		if (sheetNumber < 0)
+			return templates;
+		try {
+			if (workbook == null)
+				openWorkBook(filePathName);
+			int currentRowNumber = getTemplateSheetCurrentRowNumber(sheetNumber);
+			templates.addAll(readData(workbook, sheetNumber, currentRowNumber, readSize));
+			setTemplateSheetCurrentRowNumber(sheetNumber, currentRowNumber + templates.size());
+		} catch (Exception e) {
+			logger.warn(e.getMessage(), e);
+		}
+		return templates;
+	}
+
+	/**
+	 * Read template data at sheet sheetNumber and filling data into given template array.
+	 *
+	 * @param sheetNumber sheet number
+	 * @param templates   given template array [not null]
+	 * @return read size
+	 */
+	public int readExcelData(int sheetNumber, Template[] templates) {
+		int readCount = 0;
+		if (templates == null)
+			return readCount;
+		if (templates.length == 0)
+			return readCount;
+		try {
+			if (workbook == null)
+				openWorkBook(filePathName);
+			int arrayLength = templates.length;
+			int currentRowNumber = getTemplateSheetCurrentRowNumber(sheetNumber);
+			List<Template> templateList = readData(workbook, sheetNumber, currentRowNumber, arrayLength);
+			if (templateList == null)
+				return readCount;
+			readCount = templateList.size();
+			if (readCount != 0)
+				setTemplateSheetCurrentRowNumber(sheetNumber, currentRowNumber + readCount);
+			for (int i = 0; i < readCount; i++) {
+				templates[i] = templateList.get(i);
+			}
+		} catch (Exception e) {
+			logger.warn(e.getMessage(), e);
+		}
+		return readCount;
+	}
+
+	/**
+	 * Read type data at sheet sheetNumber.
+	 *
+	 * @param sheetNumber sheet number
+	 * @param readSize    given size to read
+	 * @param clazz       given type
+	 * @return type data list of given size
+	 */
+	public <T> List<T> readExcelData(int sheetNumber, int readSize, Class<T> clazz) {
+		List<T> tList = new ArrayList<>();
+		if (readSize <= 0)
+			return tList;
+		if (sheetNumber < 0)
+			return tList;
+		try {
+			if (workbook == null)
+				openWorkBook(filePathName);
+			int currentRowNumber = getTypeSheetCurrentRowNumber(sheetNumber);
+			tList.addAll(readData(clazz, workbook, sheetNumber, currentRowNumber, readSize));
+			setTyepSheetCurrentRowNumber(sheetNumber, currentRowNumber + tList.size());
+		} catch (Exception e) {
+			logger.warn(e.getMessage());
+		}
+		return tList;
+	}
+
+	/**
+	 * Read type data at sheet sheetNumber and filling data into given type data array.
+	 *
+	 * @param sheetNumber sheet number
+	 * @param ts          given template array [not null]
+	 * @param clazz       given type
+	 * @return read size
+	 */
+	public <T> int readExcelData(int sheetNumber, T[] ts, Class<T> clazz) {
+		int readCount = 0;
+		if (ts == null)
+			return readCount;
+		if (ts.length == 0)
+			return readCount;
+		try {
+			if (workbook == null)
+				openWorkBook(filePathName);
+			int arrayLength = ts.length;
+			int currentRowNumber = getTypeSheetCurrentRowNumber(sheetNumber);
+			List<T> tList = readData(clazz, workbook, sheetNumber, currentRowNumber, arrayLength);
+			if (tList == null)
+				return readCount;
+			readCount = tList.size();
+			if (readCount != 0)
+				setTyepSheetCurrentRowNumber(sheetNumber, currentRowNumber + readCount);
+			for (int i = 0; i < readCount; i++) {
+				ts[i] = tList.get(i);
+			}
+		} catch (Exception e) {
+			logger.warn(e.getMessage(), e);
+		}
+		return readCount;
+	}
+
+	private boolean openWorkBook(String filePathName) {
 		try {
 			if (workbook != null) {
 				close(workbook);
@@ -40,6 +247,36 @@ public class ComplexReader extends AbstractReader {
 		}
 	}
 
+	/**
+	 * Refresh reader index of the excel file sheet position (row number).
+	 * <p>
+	 * <pre>
+	 *     Using for re-read the excel file (all sheet) from the begin row.
+	 * </pre>
+	 *
+	 * @return true/false
+	 */
+	public boolean refresh() {
+		try {
+			for (Integer integer : templateSheetCurrentRowNumberMap.keySet()) {
+				setTemplateSheetCurrentRowNumber(integer, 0);
+			}
+
+			for (Integer integer : typeSheetCurrentRowNumberMap.keySet()) {
+				setTyepSheetCurrentRowNumber(integer, 0);
+			}
+			return true;
+		} catch (Exception e) {
+			logger.warn(e.getMessage(), e);
+			return false;
+		}
+	}
+
+	/**
+	 * Close the workbook.
+	 *
+	 * @return true/false
+	 */
 	public boolean close() {
 		if (workbook == null)
 			return false;
---
+++ src/test/java/xuyihao/java2excel/ComplexReaderTest.java
@@ -0,0 +1,78 @@
+package xuyihao.java2excel;
+
+import org.junit.Assert;
+import org.junit.Test;
+import xuyihao.java2excel.core.entity.model.Template;
+import xuyihao.java2excel.entity.Student;
+import xuyihao.java2excel.util.JsonUtils;
+
+import java.util.List;
+
+/**
+ * Created by xuyh at 2018/1/8 14:25.
+ */
+public class ComplexReaderTest {
+	@Test
+	public void testTemplateData() {
+		ComplexReader reader = new ComplexReader("D:\\complex.xlsx");
+		Template template = reader.readExcelTemplate(0);
+		Assert.assertNotNull(template);
+		System.out.println(JsonUtils.obj2JsonStr(template));
+		int count = reader.readExcelDataCount(0);
+		Assert.assertNotEquals(0, count);
+		System.out.println(count);
+
+		System.out.println("\n----------------------------------------------------------------------read list");
+		int listCount;
+		do {
+			List<Template> templates = reader.readExcelData(0, 10);
+			listCount = templates.size();
+			templates.forEach(template1 -> System.out.println(JsonUtils.obj2JsonStr(template1)));
+		} while (listCount > 0);
+
+		reader.refresh();
+
+		System.out.println("\n----------------------------------------------------------------------read array");
+		Template[] templates = new Template[10];
+		int arrayCount;
+		while ((arrayCount = reader.readExcelData(0, templates)) > 0) {
+			for (int i = 0; i < arrayCount; i++) {
+				System.out.println(JsonUtils.obj2JsonStr(templates[i]));
+			}
+		}
+
+		reader.close();
+	}
+
+	@Test
+	public void testTypeData() {
+		ComplexReader reader = new ComplexReader("D:\\complex.xlsx");
+		Class<?> clazz = reader.readExcelJavaClass(1);
+		Assert.assertNotNull(clazz);
+		System.out.println(JsonUtils.obj2JsonStr(clazz));
+		int count = reader.readExcelDataCount(1);
+		Assert.assertNotEquals(1, count);
+		System.out.println(count);
+
+		System.out.println("\n----------------------------------------------------------------------read list");
+		int listCount;
+		do {
+			List<Student> studentList = reader.readExcelData(1, 10, Student.class);
+			listCount = studentList.size();
+			studentList.forEach(student -> System.out.println(JsonUtils.obj2JsonStr(student)));
+		} while (listCount > 0);
+
+		reader.refresh();
+
+		System.out.println("\n----------------------------------------------------------------------read array");
+		Student[] students = new Student[10];
+		int arrayCount;
+		while ((arrayCount = reader.readExcelData(1, students, Student.class)) > 0) {
+			for (int i = 0; i < arrayCount; i++) {
+				System.out.println(JsonUtils.obj2JsonStr(students[i]));
+			}
+		}
+
+		reader.close();
+	}
+}
